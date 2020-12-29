e417ab4e3604f44a326984579fe7f8b5d3a5ae0b
je-pa <jieun0502@gmail.com>
2020-12-29 15:50:16 +0900

    20.12.29. JAVA1 if문

--- com.kita.first/src/com/kita/first/Printf.java
+++ com.kita.first/src/com/kita/first/Printf.java
@@ -32,5 +32,10 @@ public class Printf {
 		
 		System.out.printf("올해로 %d살이 된 %s는 \n알바를 시작하여 매달 %,d을 받게 되었다.\n",age,name,salary);
 		System.out.printf("%s은 월급의 %d%%인 %d원을 떼어\n%c저축은행의 연 이자율 %.2f%%짜리 적금에 넣기로 결심했다.",name,percent,savings,bank,rateOfInterest);
+		
+		boolean bool = true;
+		System.out.printf("%b", bool);
+		System.out.printf("%s",bool);
+				
 	}
 }
