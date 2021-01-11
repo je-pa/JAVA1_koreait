cd4c91df97957287b37a65b440d56b6a3a287f64
je-pa <jieun0502@gmail.com>
2021-01-11 15:43:34 +0900

    21.01.11_과목합계평균

--- com.kita.first/src/com/kita/first/practice/Practice28.java
+++ com.kita.first/src/com/kita/first/practice/Practice28.java
@@ -2,6 +2,31 @@ package com.kita.first.practice;
 
 public class Practice28 {
 	public static void main(String[] args) {
+		/*
+		 * 국어합계: ?, 평균: ? 00.0
+		 * 수학합계: ?, 평균: ? 
+		 * 영어합계: ?, 평균: ? 
+		 * 전체합계: ?, 평균: ? 
+		 */		
 		
+		int[][] arr= {
+				{90,100,88}, //국어
+				{34,99,45}, //수학
+				{89,65,74} //영어
+		};
+		String[] clsArr= {"국어","수학","영어"};
+		int sum=0;
+		int count=0;
+		int[] sumArr=new int[clsArr.length];
+		
+		for(int i=0;i<arr.length;i++) {
+			for(int j=0;j<arr[i].length;j++,count++) {
+				sumArr[i]+=arr[i][j];
+			}
+			System.out.printf("%s합계: %d 평균: %.1f\n",clsArr[i],sumArr[i],(float)sumArr[i]/arr[i].length);
+			sum+=sumArr[i];
+		}
+
+		System.out.printf("전체합계: %d 평균: %.1f\n",sum,(float)sum/count);
 	}
 }
