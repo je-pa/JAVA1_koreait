de40294d463d7c90f50f258f12cb9ddf9e50799f
je-pa <jieun0502@gmail.com>
2021-01-18 15:49:33 +0900

    21.01.18_Phone

--- com.kita.first/src/com/kita/first/level2/Phone.java
+++ com.kita.first/src/com/kita/first/level2/Phone.java
@@ -5,7 +5,7 @@ public class Phone {
 	
 	
 	//필드 (속성)이름, 색깔, ..밑에 애들은 필드!
-	String company = "삼성";
+	String company;
 	String name;
 	String color;
 	int memory;
@@ -13,20 +13,22 @@ public class Phone {
 	//생성자 필드를 초기화해주는 역할 / 초기값 //Calc(타입) a = new Calc()(생성자) 에서 Calc()를 생성자라고함 //클래스는 하나이상의 클래스를 가지고있다. 생성시켜주는 역할  //return타입없음
 
 	Phone() { } //기본생성자 : 따로 정의한 생성자가 없을때 컴파일러가 자동 정의해줌 //필드를 초기화, 클래스랑 이름 같음
-	Phone(String name){
-		this(name,"검정색",200);//this()는 생성자의 맨 첫번째 줄에만 사용가능
+	Phone(String company){
+		this(company,"휴대폰","검정색",200);//this()는 생성자의 맨 첫번째 줄에만 사용가능
 	}
-	Phone(String name, String color){
-		this(name,color,200);
+	Phone(String company, String name, String color){
+		this(company,name,color,200);
 	}
-	Phone(String name, String color, int memory){ 
+	Phone(String company, String name, String color, int memory){ 
+		this.company =company;
 		this.name =name; 
 		this.color =color;
 		this.memory =memory;
 	}
 	
 	//메소드 (동작)전화 문자 카메라.. . 메소드 안의 제일 바깥에 있는 변수는 전역변수
-	String sendMsg() {
+	String sendMsg(String str) {
+		System.out.printf("%s",str);
 		return "";
 	}
 	void call() {
--- com.kita.first/src/com/kita/first/level2/PhoneTest.java
+++ com.kita.first/src/com/kita/first/level2/PhoneTest.java
@@ -2,17 +2,25 @@ package com.kita.first.level2;
 
 public class PhoneTest { 
 	public static void main(String[] args) {
-		Phone myPhone = new Phone("갤럭시 S21","노랑색",300);
-		Phone yourPhone = new Phone("갤럭시 S20","빨간색",300);
-		System.out.println(myPhone.name);
-		System.out.println(yourPhone.name);
+		GalaxyPhone sNote20=new GalaxyPhone("s노트20","파란색",300,"갤럭시");
+		System.out.println(sNote20.ai_secr);
+		sNote20.sendMsg("안녕");
 		
-		Phone phone1 = new Phone();
-		System.out.println(phone1.name);
-		Phone phone2 = new Phone("갤럭시 A80");
-		System.out.println(phone2.name);
-		System.out.println(phone1.color);
-		System.out.println(phone2.color);
-		System.out.println(phone2.memory);
+		System.out.println("회사: "+sNote20.company);
+		System.out.println("기종명: "+sNote20.name);
+		System.out.println("색깔: "+sNote20.color);
+		System.out.println("메모리: "+sNote20.memory);
+//		Phone myPhone = new Phone("갤럭시 S21","노랑색",300);
+//		Phone yourPhone = new Phone("갤럭시 S20","빨간색",300);
+//		System.out.println(myPhone.name);
+//		System.out.println(yourPhone.name);
+//		
+//		Phone phone1 = new Phone();
+//		System.out.println(phone1.name);
+//		Phone phone2 = new Phone("갤럭시 A80");
+//		System.out.println(phone2.name);
+//		System.out.println(phone1.color);
+//		System.out.println(phone2.color);
+//		System.out.println(phone2.memory);
 	}
 }
