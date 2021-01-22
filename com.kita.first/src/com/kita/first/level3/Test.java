6f46599d73167d02d2f5df7cc6da2c403a5364f9
je-pa <jieun0502@gmail.com>
2021-01-22 15:51:01 +0900

    21.01.22_Game

---
+++ com.kita.first/src/com/kita/first/level3/Test.java
@@ -0,0 +1,56 @@
+package com.kita.first.level3;
+
+public class Test  {
+	public static void main(String[] args) {
+		ProGamer p1 =new ProGamer();
+		Game game =new Game();
+		p1.playGame(game);
+		
+		StardewValley sDew =new StardewValley();
+		p1.playGame(sDew);
+		
+		Lol lol =new Lol();
+		p1.playGame(lol);
+		
+//		Student minsu =new Student();
+//		
+//		for(int i=1; i<=5; i++) {
+//			int result =minsu.readBooks(); //5번 호출
+//			
+//			switch(result) {
+//			case 1: //minsu가 book1을 다 읽었을 경우
+//				minsu.book1 =new ComicBook(8);
+//				System.out.println("새만화책을 가져왔다");
+//				break;
+//			case 2:
+//				minsu.book2 =new MajorTextBook(7);
+//				System.out.println("새 전공서적을 가져왔다");
+//				break;
+//			case 3:
+//				minsu.book1 =new Book(3);
+//				System.out.println("새 책을 가져왔다");
+//				break;
+//			}
+//			System.out.println("----------------");
+//		}
+		
+//		Animal animal =new Animal();
+//		Cat cat = new Cat();
+//		
+//		animal.crying();
+//		
+//		animal = new Animal();
+//		animal = cat; //Cat extends Animal 되있기 때문에 자식클래스는 부모클래스로 자동형변환
+//						// 넣음으로서 cat의 주소를  animal 주소에 넣는 것이다. 그러므로 animal 주소는 사라지게 되고 animal은 Cat을 가르키게 된다.
+//		animal.crying();
+//		
+//		animal = new Dog();
+//		animal.crying();
+////		animal.barking(); //꼭 부모클래스에만 있는 메소드만 접근이 가능
+//		
+//		
+////		System.out.println(animal);
+////		System.out.println(cat);
+////		System.out.println(animal == cat);
+	}
+}
